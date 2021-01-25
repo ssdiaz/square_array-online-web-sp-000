1c93c3a1fc2747421eff8c716d0518c5e92a34e3
Sam D <samidiaz3@gmail.com>
2021-01-25 03:08:18 +0000

    Automatically backed up by Learn

--- square_array.rb
+++ square_array.rb
@@ -1,7 +1,7 @@
 def square_array(array)
   # your code here
-  array.each do |array|
-    array**2.inspect
-    puts "ha"
+  array.select do |array|
+    squared_number = array**2
+    puts squared_number
   end
 end
