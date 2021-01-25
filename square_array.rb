d2f690b4a01b4524d1cc509c74d039b58431cc1d
Sam D <samidiaz3@gmail.com>
2021-01-25 03:14:40 +0000

    Automatically backed up by Learn

--- square_array.rb
+++ square_array.rb
@@ -1,7 +1,16 @@
 def square_array(array)
   # your code here
-  array.select do |array|
+  array.inject do |array|
     squared_number = array**2
-    puts squared_number
+    #puts squared_number
+    square_array = Array.new
+    square_array << squared_number
   end
+
+  
 end
+
+
+
+
+# square_array(numbers)
